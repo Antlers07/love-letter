a72166cee2896a3bda04401955effd4449bb5600
Antlers <tzishelim@gmail.com>
2026-08-19 17:47:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -715,7 +715,7 @@ function startTyping() {
         if (index < loveMessage.length) {
             element.textContent += loveMessage.charAt(index);
             index++;
-            setTimeout(type, 85);
+            setTimeout(type, 100);
         }
     }
 
